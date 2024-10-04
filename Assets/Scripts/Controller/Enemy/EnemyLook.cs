bebcbe466469846cddd5d91c5b67af0945007a46
STERN <resendo13@gmail.com>
2024-10-04 19:04:11 +0900

    적 탐지 UI 제작
    적 탐지 UI 제작
    - 플레이어가 일정 거리 도달 시, '?' 상태
    - 플레이어가 일정 거리 도달 후에 일정 시간이 지났을 경우, '!' 상태
    - 머리에 알맞는 이미지(!, ?)를 띄움
    - 아직 이미지를 노란색, 빨간색으로만 표현함

--- Assets/Scripts/Controller/Enemy/EnemyLook.cs
+++ Assets/Scripts/Controller/Enemy/EnemyLook.cs
@@ -10,6 +10,9 @@ public class EnemyLook : MonoBehaviour
 
     float _timeDelta = 0f;
     [SerializeField] float detectionRate;
+    // Detection UI
+    [SerializeField] GameObject weakDetectionUI; // '?', player find
+    [SerializeField] GameObject strongDetectionUI; // '!', player find, and after a certain period of time
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +20,10 @@ public class EnemyLook : MonoBehaviour
         _status = GetComponent<EnemyStatus>();
 
         GameManager.Enemy.UpdateDelegate += StateUpdate;
+
+        // Deactivate detection UI
+        weakDetectionUI.SetActive(false);
+        strongDetectionUI.SetActive(false);
     }
 
     bool Searching(out float distanceToPlayer)
@@ -109,6 +116,10 @@ public class EnemyLook : MonoBehaviour
 
             //발각 상태가 되면 10초가 되서 5초의 유예시간을 줌
             if (_timeDelta > 10f || _status.state == EnemyState.Capture) { _timeDelta = 10f; return; }
+
+            // Mark the '?', when the enemy found the player
+            weakDetectionUI.SetActive(true);
+            strongDetectionUI.SetActive(false);
         }
         else
         {
@@ -119,14 +130,26 @@ public class EnemyLook : MonoBehaviour
         if (_timeDelta > _status.captureTime)
         {
             _status.state = EnemyState.Capture;
+
+            // Mark the '!', when the enemy completely found the player
+            weakDetectionUI.SetActive(false);
+            strongDetectionUI.SetActive(true);
         }
         else if (_timeDelta > _status.boundaryTime)
         {
             _status.state = EnemyState.Boundary;
+
+            // Mark the '?', when the detection state is maintained
+            weakDetectionUI.SetActive(true);
+            strongDetectionUI.SetActive(false);
         }
         else
         {
             _status.state = EnemyState.Idle;
+
+            // Nothing, hide entire UIs.
+            weakDetectionUI.SetActive(false);
+            strongDetectionUI.SetActive(false);
         }
     }
     void CurvedCheck(Vector3 dir)

501520f8d57b98d9ee114ecbd69d832a0bcbf070
STERN <resendo13@gmail.com>
2024-10-06 16:54:42 +0900

    SceneLoader.cs 버그로 인해 다시 추가
    SceneLoader.cs 버그로 인해 다시 추가

--- Assets/Scripts/MainMenuUI/SceneLoader.cs
+++ Assets/Scripts/MainMenuUI/SceneLoader.cs
@@ -5,13 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-    // ¾À ÀÌ¸§À¸·Î ¾À ·Îµù
     public void LoadSceneByName(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
     }
 
-    // ¾À ¹øÈ£·Î ¾À ·Îµù
     public void LoadSceneByIndex(int sceneIndex)
     {
         SceneManager.LoadScene(sceneIndex);

5ac1dec4883a7ddfa4a860d849ad735b5a301923
STERN <resendo13@gmail.com>
2024-10-10 18:32:49 +0900

    PlayerAttack 스크립트에서 암살 관련 코드 제거
    PlayerAttack 스크립트에서 암살 관련 코드 제거
    - 중복으로 인한 제거

--- Assets/Scripts/Controller/Player/PlayerAttack.cs
+++ Assets/Scripts/Controller/Player/PlayerAttack.cs
@@ -11,62 +11,15 @@ public class PlayerAttack : MonoBehaviour
     //[SerializeField] GameObject Bullet;
     [SerializeField] Transform target; //공격 대상 지점
     [SerializeField] Transform firePoint; //공격 시작 지점
-
-    // Stealth UI
-    [SerializeField] float stealthRange =  0.7f;
-    [SerializeField] KeyCode stealthInteractionKey = KeyCode.F;
-    [SerializeField] GameObject stealthUI;
-
+    
     bool reloadingTrigger = false;
 
-    // Start is called before the first frame update
     void Start()
     {
         _status = GetComponent<PlayerStatus>();
 
         GameManager.Input.InputDelegate += Fire;
         GameManager.Input.InputDelegate += ReLoad;
-        GameManager.Input.InputDelegate += Stealth;
-    }
-
-    public void Stealth()
-    {
-        // Enemy tag가 되어 있는 오브젝트를 찾음
-        Collider[] enemies = Physics.OverlapSphere(transform.position, stealthRange, LayerMask.GetMask("Enemy"));
-
-        foreach (var enemy in enemies)
-        {
-            var enemyStatus = enemy.GetComponent<EnemyStatus>();
-            if (enemyStatus.Hp == 0)
-            {
-                Debug.Log("EnemyStatus is null!");
-                continue; // 다음 적으로 넘어가기
-            }
-
-            Vector3 directionToEnemy = (enemy.transform.position - transform.position).normalized; 
-            float dotProduct = Vector3.Dot(transform.forward, directionToEnemy); // 플레이어가 적을 바라보고 있는지 확인
-
-            if (dotProduct > 0) // 플레이어가 적의 등을 보는 경우
-            {
-                // 스텔스 킬 UI 표시 및 상호작용 처리
-                ShowStealthUI(true);
-
-                if (Input.GetKeyDown(stealthInteractionKey))
-                {
-                    enemy.GetComponent<EnemyStatus>().Hp = 0; // 적의 체력 0으로 만듦
-                    ShowStealthUI(false); // UI 숨김
-                    Debug.Log("Enemy Hp = 0");
-                }
-                return;
-            }
-        }
-
-        ShowStealthUI(false); // 적이 없으면 UI 숨김
-    }
-
-    void ShowStealthUI(bool show)
-    {
-        stealthUI.SetActive(show); // 스텔스 텍스트 UI 표시/숨기기
     }
 
     void Fire()
@@ -146,6 +99,5 @@ public class PlayerAttack : MonoBehaviour
     public void Clear()
     {
         GameManager.Input.InputDelegate -= Fire;
-        GameManager.Input.InputDelegate -= Stealth;
     }
 }

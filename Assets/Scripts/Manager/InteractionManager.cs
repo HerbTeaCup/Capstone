f72000af76345e567f34bb69f6489267e033784d
STERN <resendo13@gmail.com>
2024-11-04 15:08:51 +0900

    수정

--- Assets/Scripts/Manager/InteractionManager.cs
+++ Assets/Scripts/Manager/InteractionManager.cs
@@ -5,18 +5,14 @@ using UnityEngine;
 public class InteractionManager : MonoBehaviour
 {
     public Transform player;
-
     public GameObject interactionUI;
     public GameObject objectToActivate;
-    
     public Outline outline;
 
     public float interactionDistance = 5f;
     private bool inRange = false;
-
     public KeyCode interactionKey = KeyCode.F;
-
-    public Vector3 offset = new Vector3(0, 1f, 0);
+    public Vector3 offset = new Vector3(0, 0f, 0); // 머리 위로 올리기 위해 Y 값을 조정
     private Camera mainCamera;
 
     void Start()
@@ -39,8 +35,15 @@ public class InteractionManager : MonoBehaviour
                 interactionUI.SetActive(true);
             }
 
-            Vector3 screenPosition = mainCamera.WorldToScreenPoint(transform.position + offset);
-            interactionUI.transform.position = screenPosition;
+            // 월드 공간에서 UI의 위치 설정
+            interactionUI.transform.position = transform.position + offset;
+            interactionUI.transform.LookAt(mainCamera.transform); // UI가 카메라를 바라보도록 설정
+
+            // UI의 좌우반전을 위해 로컬 스케일 조정
+            Vector3 scale = interactionUI.transform.localScale;
+            scale.x = -Mathf.Abs(scale.x); // X축을 반전
+            interactionUI.transform.localScale = scale;
+
             if (Input.GetKeyDown(interactionKey))
             {
                 ActivateObject();
@@ -64,6 +67,7 @@ public class InteractionManager : MonoBehaviour
             outline.enabled = show;
         }
     }
+
     void ActivateObject()
     {
         if (objectToActivate != null)
